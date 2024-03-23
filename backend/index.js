ddadc6ca0ffd9e9393a554ee1b66d6fcb55fe924
saroj-mali <sarojmali6090@gmail.com>
2024-03-23 14:31:08 +0530

    refactor the code

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,51 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const connectDB = require("./config/database");
-const userRoutes = require("./routes/userRoutes");
-const authRoutes = require("./routes/authRoutes");
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
 
-require("dotenv").config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const __dirname = path.resolve();
+
+const app = express();
+
 app.use(express.json());
-app.use(cors());
+
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-  res.status(statusCode).json({
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
   });
-});
-
-app.get("/", (req, res) => {
-  res.status(200).json("This is the home page 🏠");
-});
-
-app.listen(8000, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-  connectDB();
 });
---
+++ frontend/src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCIaZ2je-9Mk5qi5Wf6b0oLeGtR9M0PgY",
+  authDomain: "real-estate-2a1be.firebaseapp.com",
+  projectId: "real-estate-2a1be",
+  storageBucket: "real-estate-2a1be.appspot.com",
+  messagingSenderId: "262475355856",
+  appId: "1:262475355856:web:7e20e6497322aec70770d5",
+};
+
+export const app = initializeApp(firebaseConfig);
